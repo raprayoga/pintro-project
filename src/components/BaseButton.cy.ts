37799ebbeb4084e0b2a93045423d1954dafc3102
raprayoga <prayogarafie@gmail.com>
2023-08-25 11:12:13 +0700

    component button with stories & test

--- src/components/BaseButton.cy.ts
+++ src/components/BaseButton.cy.ts
@@ -5,10 +5,16 @@ describe("<BaseButton />", () => {
     // see: https://on.cypress.io/mounting-vue
     cy.mount(BaseButton, {
       props: {
-        label: "BUTTON NIH BOSS",
-        variant: "white",
-        class: "w-[300px]",
+        variant: "primary",
+        size: "sm",
+      },
+      slots: {
+        default: `Submit`,
       },
     })
+
+    cy.get('[data-cy="base-button-element"]')
+      .should("have.class", "bg-primary rounded-md px-2 py-1")
+      .should("have.text", "Submit")
   })
 })
